d85ea330d975eebb7bac4e44933034d4154517e8
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-05-07 01:21:35 +0000

    Fixed Service Starting Issue

--- application/tmp/webister/files/grabpass.php
+++ application/tmp/webister/files/grabpass.php
@@ -7,6 +7,20 @@
  *
  *
  */
-
+shell_exec("cd /var/webister/interface && sudo php -S 0.0.0.0:8081 &>/dev/null &");
 include '/var/webister/interface/config.php';
-echo $pass;
+ $mysqli = new mysqli();
+    $con = mysqli_connect("$host", "$user", "$pass", "$data"); 
+    $sql = "SELECT port \n"
+    ."FROM `Users`";
+
+if ($result = mysqli_query($con, $sql)) {
+    // Fetch one and one row
+    while ($row = mysqli_fetch_row($result)) {
+        if (!file_exists("/var/webister/" . $row[0])) {
+            echo "Creating Port File " . $row[0];
+            mkdir("/var/webister/" . $row[0]);
+        }
+shell_exec("cd /var/webister/ && php -S 0.0.0.0:" . $row[0]. " &>/dev/null &");
+    }
+}
