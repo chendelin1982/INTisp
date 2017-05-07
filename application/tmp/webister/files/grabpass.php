22fe4a45c3aa0b9aba7b727e033b60b12bbc1f93
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-05-07 15:29:29 +0000

    Accidentally forgot port

--- application/tmp/webister/files/grabpass.php
+++ application/tmp/webister/files/grabpass.php
@@ -7,6 +7,7 @@
  *
  *
  */
+ shell_exec("sudo service apache2 stop");
 shell_exec("cd /var/webister/interface && sudo php -S 0.0.0.0:8081 &>/dev/null &");
 include '/var/webister/interface/config.php';
  $mysqli = new mysqli();
@@ -21,6 +22,6 @@ if ($result = mysqli_query($con, $sql)) {
             echo "Creating Port File " . $row[0];
             mkdir("/var/webister/" . $row[0]);
         }
-shell_exec("cd /var/webister/ && php -S 0.0.0.0:" . $row[0]. " &>/dev/null &");
+shell_exec("cd /var/webister/" . $row[0] . "/ && php -S 0.0.0.0:" . $row[0]. " &>/dev/null &");
     }
 }
