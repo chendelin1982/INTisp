0f53d78445ff36b89a10952a897da428ef5f9a24
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-05-08 13:15:35 +0000

    Fix FTP Server

--- application/tmp/webister/interface/newserv.php
+++ application/tmp/webister/interface/newserv.php
@@ -83,6 +83,52 @@ VALUES ('".rand(10000, 99999)."', '".$username."', '".sha1($password)."','0','".
         shell_exec('sudo nohup killall python > exhibitor.out 2>&1 &');
         mkdir("/var/webister/" . $port);
         shell_exec("cd /var/webister/" . $port . "/ && sudo nohup php -S 0.0.0.0:" . $port. " > exhibitor.out 2>&1 &");
+    
+
+/*
+ * Adaclare Technologies
+ *
+ * Webister Hosting Software
+ *
+ *
+ */
+ $mysqli = new mysqli();
+    $con = mysqli_connect("$host", "$user", "$pass", "$data"); 
+    $sql = "SELECT * \n"
+    ."FROM `Users`";
+
+
+$file = "from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+authorizer = DummyAuthorizer()
+";
+
+$file_end = 'handler = FTPHandler
+handler.banner = "Webister By Adaclare"
+handler.authorizer = authorizer
+server = FTPServer(("0.0.0.0", 21), handler)
+server.max_cons_per_ip = 5
+server.serve_forever()';
+
+
+$u = "";
+
+if ($result = mysqli_query($con, $sql)) {
+    // Fetch one and one row
+    while ($row = mysqli_fetch_row($result)) {
+        if (!file_exists("/var/webister/" . $row[5])) {
+            echo "Creating Port File " . $row[5];
+            mkdir("/var/webister/" . $row[5]);
+        }
+$u = $u . 'authorizer.add_user("' . $row[1] . '", "' . $row[2] . '", "/var/webister/' . $row[5] . '", perm="elradfmw")
+
+';
+    }
+}
+$fttl = $file . $u . $file_end;
+file_put_contents("/var/webister/ftpserv.py",$fttl);
+
         shell_exec("python /var/webister/ftpserv.py > exhibitor.out 2>&1 &");
         $returnval = $returnval.'<br>Done! Please restart webister';
         header('Location: ?pa='.urlencode($returnval));
