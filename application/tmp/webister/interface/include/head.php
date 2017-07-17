64e3c76e7496cd3d16f6feb1071f6925552c2527
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-07-17 23:20:01 +0000

    Added Salts

--- application/tmp/webister/interface/include/head.php
+++ application/tmp/webister/interface/include/head.php
@@ -81,6 +81,7 @@ $result = mysqli_query($con, $sql);
     }
 </script>
 <script type='text/javascript' crossorigin='anonymous' src='https://cdn.errlytics.com/errlytics.min.js'></script>
+
 	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
             <?php if (file_Get_contents("data/theme") == "default") { ?>
 				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
