9197b608751e4c4482760fbbcf9b6a9b498addc8
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-07-18 23:58:49 +0000

    User Tracking

--- application/tmp/webister/interface/include/head.php
+++ application/tmp/webister/interface/include/head.php
@@ -75,12 +75,16 @@ $result = mysqli_query($con, $sql);
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 	<link rel="icon" href="https://www.kalzediahosting.com/assets/kh.png">
 	<link rel="stylesheet" type="text/css" href="style/tour.css">
-<script type='text/javascript'>
-    window._errLyticsConfig = {
-        apiKey: 'grAwnyKPXuPPoZpgvRohOy2zfDSwVHpOEp6SOo6gcFU'
-    }
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-61941734-2', 'auto');
+  ga('send', 'pageview');
+
 </script>
-<script type='text/javascript' crossorigin='anonymous' src='https://cdn.errlytics.com/errlytics.min.js'></script>
 
 	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
             <?php if (file_Get_contents("data/theme") == "default") { ?>
