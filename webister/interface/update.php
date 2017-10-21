44c5bf48eefbef72af4e9dbcf20df1d1a7dc3b7b
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-10-21 17:15:57 +0000

    New Update System

--- webister/interface/update.php
+++ webister/interface/update.php
@@ -1,19 +1,39 @@
 <?php session_start(); require 'include/head.php';onlyadmin();
-if (isset($_POST["fileToUpload"])) {
-    file_put_contents("patch.deb", file_get_contents($_POST["fileToUpload"]));
-       $out = exec("cd /var/webister/interface && dpkg -i patch.deb > log.log");
-       echo $out;
-}
-
-
+ini_set('max_execution_time',60);
 ?>
-<h1>Update to New Version</h1>
-<p>You can find some patches over <a href="https://circleci.com/gh/alwaysontop617/webister/tree/master">Here</a>. You will need to get the url of patch.deb. Once you get the url paste it below and wait for it to apply.</p>
-<form action="#" method="post" enctype="multipart/form-data">
-    Enter url to patch:
-    <input type="text" name="fileToUpload" id="fileToUpload">
-    <input type="submit" value="Upload Image" name="submit">
-</form>
+ <div class="content-wrapper">
+            <div class="container-fluid">
+
+                <div class="row">
+                    <div class="col-md-12">
+
+                        <h2 class="page-title">Updates</h2>
+<?php
+$current = file_get_contents("data/version");
+$getVersions = file_get_contents("https://dl.adaclare.com/main/intisp/curversion.txt");
+if ($getVersions != '')
+{
+    echo '<p>CURRENT VERSION: '.$current.'</p>';
+    echo '<p>Reading Current Releases List</p>';
+    if ($current != $getVersions) {
+        echo '<p>New Update Found: v'.$getVersions.'</p>';
+           if ( !is_file('data/wb-'.$getVersions.'.zip' )) {
+                echo '<p>Downloading New Update</p>';
+                $newUpdate = file_get_contents('https://dl.adaclare.com/main/intisp/wb-'.$getVersions.'.zip');
+ 
+                $dlHandler = fopen('data/wb-'.$getVersions.'.zip', 'w');
+                if ( !fwrite($dlHandler, $newUpdate) ) { echo '<p>Could not save new update. Operation aborted.</p>'; exit(); }
+                fclose($dlHandler);
+                echo '<p>Update Downloaded And Saved</p>';
+            } else echo '<p>Update already downloaded.</p>';   
+    } else {
+        echo '<p>&raquo; No update is available.</p>';
+    }
+} else {
+    echo '<p>Could not find latest realeases.</p>';
+}
+?>
+
 <?php
 require 'include/footer.php';
 ?>
