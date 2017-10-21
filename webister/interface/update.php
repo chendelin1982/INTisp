926db4b8600bc3ce7530d53b379a74f7ba229b6c
Thomas Wilbur <alwaysonestepahead617@gmail.com>
2017-10-21 18:06:06 +0000

    Update System Working

--- webister/interface/update.php
+++ webister/interface/update.php
@@ -1,5 +1,6 @@
 <?php session_start(); require 'include/head.php';onlyadmin();
 ini_set('max_execution_time',60);
+error_reporting(E_ALL);
 ?>
  <div class="content-wrapper">
             <div class="container-fluid">
@@ -26,6 +27,16 @@ if ($getVersions != '')
                 fclose($dlHandler);
                 echo '<p>Update Downloaded And Saved</p>';
             } else echo '<p>Update already downloaded.</p>';   
+             if ($_GET['doUpdate'] == true) {
+                 echo "Opening " . 'data/wb-'.$getVersions.'.zip...<pre>';
+            echo shell_exec('unzip -o /var/www/html/interface/data/wb-'.$getVersions.'.zip -d /var/www/html/interface/');
+               echo "</pre>";
+                $updated = TRUE;
+                file_put_contents("data/version",$getVersions);
+                 echo '<p class="success">&raquo; Webister Updated to v'.$getVersions.'</p>';
+             } else {
+                  echo '<p>Update ready. <a href="?doUpdate=true">&raquo; Install Now?</a></p>';
+             }
     } else {
         echo '<p>&raquo; No update is available.</p>';
     }
